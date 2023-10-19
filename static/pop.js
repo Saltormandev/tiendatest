bb69577b41b494c3b5d87afc7163b1ca6a662861
SaltormanGamedev <saltormandev@hotmail.com>
2023-10-19 12:40:43 -0400

    git add
    test

---
+++ static/pop.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
+  document.getElementById("showPopup").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "block";
+  });
+
+  document.getElementById("closePopup").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "none";
+  });
+
+
+
+});
